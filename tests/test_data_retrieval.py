5b038486f854d9c52ac6ff783968336384a7edad
odococo <ivan_lamperti@outlook.it>
2020-06-06 18:39:33 +0200

    test 1

---
+++ tests/test_data_retrieval.py
@@ -0,0 +1,7 @@
+from code.data_retrieval import data_retrieval
+
+
+def test_data_retrieval():
+    cell_line = 'HepG2'
+    region = 'promoters'
+    data_retrieval(cell_line=cell_line, region=region)
