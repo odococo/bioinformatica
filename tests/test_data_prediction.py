0a4e52fd717c34ac19455780348474e7cf498a97
odococo <ivan_lamperti@outlook.it>
2020-06-06 22:58:45 +0200

    two epochs

--- tests/test_data_prediction.py
+++ tests/test_data_prediction.py
@@ -2,7 +2,7 @@ from shutil import rmtree
 
 from bioinformatica.data_analysis import get_filtered_with_boruta
 from bioinformatica.data_manipulation import fit_neighbours, apply_z_scoring, drop_constant_features, drop_uncorrelated
-from bioinformatica.data_prediction import predict_epigenomics
+from bioinformatica.data_prediction import predict_epigenomics, show_barplots
 from bioinformatica.data_retrieval import data_retrieval
 from bioinformatica.defaults import set_default, get_default
 from bioinformatica.models import get_mlp_epigenomics
@@ -12,8 +12,8 @@ def test_data_prediction():
     set_default(
         cell_line='HEK293',
         region='enhancers',
-        epochs=1,
-        splits=1,
+        epochs=2,
+        splits=2,
         batch_size=1024,
         boruta_iterations=2
     )
@@ -29,10 +29,12 @@ def test_data_prediction():
     epi_models = [
         get_mlp_epigenomics()(shape, name="MLP")
     ]
-    _ = predict_epigenomics(input_data_epi.values, output_data.values.ravel(), epi_models)
+    results = predict_epigenomics(input_data_epi.values, output_data.values.ravel(), epi_models)
+    show_barplots(results, 'epi')
 
     rmtree('datasets')
-    rmtree('results_HEK293_enhancers_epi.json')
+    rmtree('epi', ignore_errors=True)
+    rmtree('results_HEK293_enhancers_epi.json', ignore_errors=True)
 
 
 if __name__ == '__main__':
